f4f8406fa53954bdb8a5564fb421813666943904
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-12-28 13:56:50 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('mongodb connected successfully');
-    } catch (error) {
-        console.log(error);
+  try {
+    const dbURI = process.env.MONGODB_URI;
+    if (!dbURI) {
+      throw new Error("MongoDB URI is not defined in the environment variables.");
     }
-}
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("mongodb connected successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
 export default connectDB;
