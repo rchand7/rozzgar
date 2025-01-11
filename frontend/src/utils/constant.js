6d6f796fa295472793a4544bc2b93c54520bfa0b
rchand7 <150305815+rchand7@users.noreply.github.com>
2025-01-11 14:49:44 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://rozzgar.shop/api/v1/user";
-export const JOB_API_END_POINT="http://rozzgar.shop/api/v1/job";
-export const APPLICATION_API_END_POINT="http://rozzgar.shop/api/v1/application";
-export const COMPANY_API_END_POINT="http://rozzgar.shop/api/v1/company";
+export const USER_API_END_POINT="https://rozzgar.shop/api/v1/user";
+export const JOB_API_END_POINT="https://rozzgar.shop/api/v1/job";
+export const APPLICATION_API_END_POINT="https://rozzgar.shop/api/v1/application";
+export const COMPANY_API_END_POINT="https://rozzgar.shop/api/v1/company";
